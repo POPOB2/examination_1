c0732457b2f37c8d247a863038a6dc9577e05b26
PB2 <nft192891@gmail.com>
2024-04-28 21:44:29 +0800

    增加台列表的分頁功能

--- back/image.php
+++ back/image.php
@@ -10,12 +10,24 @@
                     <td></td>
                 </tr>
                 <?php
-                $rows=$DB->all();
+                $all=$DB->math('count','id'); // 獲取資料總數
+                $div=3; // 每頁幾張
+                $pages=ceil($all/$div); // 頁數(ceil取數字結果的最大數)
+                $now=$_GET['p']??1; // 當前所在位置 // 網址帶值以p決定所在頁數, 若無p從第1頁開始
+                $start=($now-1)*$div; // 起始資料數(頁數)
+/* 1p+3 = 顯示1、2、3
+   3p+3 = 顯示7、8、9
+   6p+3 = 顯示16、17、18 */
+                // SELECT * FROM table limit 0,3
+                // SELECT * FROM table limit 3,3
+                // SELECT * FROM table limit 6,3
+                //第一頁:p=1、now=1、start=0,略過0筆後顯示3筆 //第二頁:p=2、now=2、start=3,略過3筆後顯示3筆 // 第三頁:p=3、now=3、start=6,略過6筆後顯示3筆
+                $rows=$DB->all(" limit $start,$div"); //撈出的資料為 //使用limit(參數1:略過的資料數、參數2:顯示的資料數量)的結果 //$start起始位置==略過前面的資料, $div顯示的資料數
                 foreach($rows as $row){
                 ?>
                 <tr>
                     <td>
-                        <img src="./img/<?=$row['img'];?>" style="width:300px; height:180px">
+                        <img src="./img/<?=$row['img'];?>" style="width:150px; height:70px">
                     </td>
                     <td>
                         <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
@@ -32,6 +44,29 @@
                 <?php } ?>
             </tbody>
         </table>
+
+        <div class="cent">
+            <?php
+                // 最小第2頁要顯示左箭頭, 2-1=1, 1>0=顯示左箭頭
+                if(($now-1) > 0){
+                    $p=$now-1;
+                    echo "<a href='?do={$Str->table}&p=$p'> < </a>"; // do=image, {變數化}後可套用至其他需要頁碼的功能
+                }
+                for($i=1; $i<=$pages; $i++){
+                    $fontsize=($now==$i)?'1.4rem':''; // 當前頁數字放大可用GET的p對比判斷
+                    // 在此利用網址帶p值給該image.php, 該p值影響上方的$now, $now影響$start的計算結果, $start影響$DB->all()的顯示計算
+                    echo "<a href='?do={$Str->table}&p=$i'; style='font-size:$fontsize; text-decoration:none';> "; // ?==跳轉至當前頁、帶值'image'和'p' , 使網頁顯示對應位置的內容 
+                    echo "$i";
+                    echo " </a>";
+                }
+                // 目前頁+1 <= 最大總頁數, 顯示右箭頭, 假設當前頁為3,3+1=4,最大頁數為3,3不<=4,不顯示右箭頭
+                if(($now+1) <= $pages){
+                    $p=$now+1;
+                    echo "<a href='?do={$Str->table}&p=$p'> > </a>";
+                }
+            ?>
+        </div>
+
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
